87f24b2eaa3c731dee3dfeb110b8c7ec43c5a251
chien <chien.dotruong@gmail.com>
2021-07-23 12:36:01 +0700

    +Add

---
+++ get_btc_price.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+import requests
+import json
+import time
+import datetime
+import platform
+import os
+import argparse
+# ----------------------------------------------------------------------------------------
+
+def parser():
+    parser = argparse.ArgumentParser(description="Get bitcoin price")
+    parser.add_argument("--time", type=int, default="30", help="Give me time to re-get new price. Ex: 10")
+    return parser.parse_args()
+
+# ----------------------------------------------------------------------------------------
+if(platform.system() == 'Linux'):
+    os.system('clear')
+if(platform.system() == 'Windows'):
+    os.system('cls')
+
+Api = 'https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT'
+
+i = 0
+args = parser()
+
+print("This script gets new BTC-USDT price in every {} seconds".format(args.time))
+print("\n" * 2)
+
+while True:
+    try:
+        url = requests.get(Api)
+        data = url.json()
+        Price = float(data['price'])
+    except:
+        print("An exception occurred")
+
+    
+    CurrentTime = str(datetime.datetime.now())
+
+    if(i%10 == 0):
+        print("{:0<.2f} <== BTC price -- Time: {}".format(Price, CurrentTime[:-7] ))
+    else:
+        print(Price)
+
+    i += 1
+    time.sleep(args.time)
